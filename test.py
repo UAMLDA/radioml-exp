7499a6c63144bd1f90f97e9a9026250c7a9c22b3
Gregory Ditzler <gregory.ditzler@gmail.com>
2021-09-08 23:53:12 +0000

    changed the experiemnt, but we are moving away from this scripts and writing scripts for each test.

--- test.py
+++ test.py
@@ -2,12 +2,15 @@
 
 from arml.utils import load_radioml
 from arml.experiments import experiment_basic_radioml, experiment_adversarial
+from arml import experiment_fgsm    
 
 # test the basic experiment
 file_path = "data/RML2016.10a_dict.pkl"
 
-experiment_adversarial(file_path=file_path)
-#experiment_basic_radioml(file_path=file_path, n_runs=5)
+# experiment_adversarial(file_path=file_path)
+# experiment_basic_radioml(file_path=file_path, n_runs=5)
+experiment_fgsm(file_path=file_path)
+
 
 # test data loader 
 #X, Y, snrs, mods, encoder = load_radioml(file_path=file_path)
