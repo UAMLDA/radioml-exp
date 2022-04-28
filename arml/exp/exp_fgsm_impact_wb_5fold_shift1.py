421fbc2307ebe2d729df452218ddd8cb0f3bee60
wenhanzhang <zwh.hfut@outlook.com>
2022-04-28 11:32:44 -0700

    Test the impact of shift with step size = 1, consider both plain and 5fold validation

--- arml/exp/exp_fgsm_impact_wb_5fold_shift1.py
+++ arml/exp/exp_fgsm_impact_wb_5fold_shift1.py
@@ -29,15 +29,16 @@ from ..adversarial_data import generate_aml_data
 
 from sklearn.model_selection import KFold
 
-def experiment_fgsm_wb(file_path:str,
+
+def experiment_fgsm_wb_5fold_shift1(file_path:str,
                     n_runs:int=5, 
                     verbose:int=1, 
                     # scenario:str='A',
                     epsilons:list=[0.00025, 0.0005],  # [0.25, 0.75, 0.125, 0.175]
                     train_params:dict={}, 
                     # train_adversary_params:dict={}, 
-                    logger_name:str='vtcnn2_FGSM_wb_shift1',
-                    output_path:str='outputs/vtcnn2_FGSM_wb_shift1_op.pkl'): 
+                    logger_name:str='vtcnn2_FGSM_5fold_wb',
+                    output_path:str='outputs/vtcnn2_FGSM_wb_5fold_op.pkl'): 
 
 
     X, Y, snrs, mods, _ = load_radioml(file_path=file_path, shuffle=True)
@@ -54,16 +55,15 @@ def experiment_fgsm_wb(file_path:str,
                         'verbose': verbose, 
                         'NHWC': [N, H, W, C],
                         'tpu': False, 
-                        'file_path': 'FGSM_CNN2_0.5_shift1.wts.h5'}
+                        'file_path': 'FGSM_CNN2_5fold.wts.h5'}
     
-     
+    
     # initialize the performances to empty 
     result_logger = FGSMPerfLogger(name=logger_name, 
                                    snrs=np.unique(snrs), 
                                    mods=np.unique(mods), 
                                    params=[train_params, train_params], 
                                    epsilons=epsilons)
-    
     kf = KFold(n_splits=n_runs)
     
     for train_index, test_index in kf.split(X): 
@@ -84,15 +84,18 @@ def experiment_fgsm_wb(file_path:str,
         # loop through the different values of epsilon and generate adversarial datasets
         for eps_index, epsilon in enumerate(epsilons): 
             Xfgsm = generate_aml_data(model_aml, Xte, Yte, {'type': 'FastGradientMethod', 'eps': epsilon})
-            # get the perturbations from the generated adversarial samples
-            perturbations = Xfgsm - Xte
-            Xfgsm_shift = np.zeros((len(perturbations)))
-            for pertub_idx, perturbation in enumerate (perturbations):
-                X
+            perturbs = Xfgsm- Xte
+            perturbs_shift = np.zeros((Xfgsm.shape[0], Xfgsm.shape[1], Xfgsm.shape[2], Xfgsm.shape[3]))
+            for i in range(len(perturbs)):
+                if i < (len(perturbs)-1):
+                    perturbs_shift[i] = perturbs[i+1]
+                else:
+                    perturbs_shift[i] = perturbs[i + 1 - len(perturbs)]
 
-
+        # reconstruact the shifted perturbed data
+            Xfgsm_shift = Xte + perturbs_shift
             for snr in np.unique(snrs_te): 
-                Yhat_fgsm = model.predict(Xfgsm[snrs_te == snr])
+                Yhat_fgsm = model.predict(Xfgsm_shift[snrs_te == snr])
                 result_logger.add_scores(Yte[snrs_te==snr], Yhat_fgsm, snr, eps_index)
         
         result_logger.increment_count()
@@ -102,7 +105,56 @@ def experiment_fgsm_wb(file_path:str,
         pickle.dump(results, open(output_path, 'wb'))
 
         
-    result_logger.finalize()
+    result_logger.finalize()  
+
+    # kf = KFold(n_splits=5)
+
+    # train_index_t = []
+    # test_index_t = []
+    # for a, b in kf.split(X):
+    #     train_index_t.append(a)
+    #     test_index_t.append(b)
+
+
+    # # use the first set to train and test the model
+    # train_index = train_index_t[0]
+    # test_index = test_index_t[0]
+    
+    # split out the training and testing data. do the sample for the modulations and snrs
+    # Xtr, Ytr, Xte, Yte, snrs_te = X[train_index], Y[train_index], X[test_index], Y[test_index], snrs[test_index]
+
+
+    
+    # model = nn_model(X=Xtr, Y=Ytr, train_param=train_params)
+    # model_aml = model
+    
+
+    # # loop through the different values of epsilon and generate adversarial datasets
+    # for eps_index, epsilon in enumerate(epsilons): 
+    #     Xfgsm = generate_aml_data(model_aml, Xte, Yte, {'type': 'FastGradientMethod', 'eps': epsilon})
+    #     perturbs = Xfgsm- Xte
+    #     perturbs_shift = np.zeros((Xfgsm.shape[0], Xfgsm.shape[1], Xfgsm.shape[2], Xfgsm.shape[3]))
+    #     for i in range(len(perturbs)):
+    #         if i < (len(perturbs)-1):
+    #             perturbs_shift[i] = perturbs[i+1]
+    #         else:
+    #             perturbs_shift[i] = perturbs[i + 1 - len(perturbs)]
+
+    #     # reconstruact the shifted perturbed data
+    #     Xfgsm_shift = Xte + perturbs_shift
+
+    #     for snr in np.unique(snrs_te): 
+    #         Yhat_fgsm = model.predict(Xfgsm[snrs_te == snr])
+    #         result_logger.add_scores(Yte[snrs_te==snr], Yhat_fgsm, snr, eps_index)
+    
+    # result_logger.increment_count()
+
+    # # # save the results to a pickle file 
+    # # results = {'result_logger': result_logger}
+    # # pickle.dump(results, open(output_path, 'wb'))
+
+        
+    # result_logger.finalize()
 
     # save the results to a pickle file 
     results = {'result_logger': result_logger}
